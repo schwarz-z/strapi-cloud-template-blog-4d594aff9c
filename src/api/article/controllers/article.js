7aff02de51197da7799e6af6ae2838713bb4f755
schwarz-z <82213925+schwarz-z@users.noreply.github.com>
2024-11-22 21:03:01 +0000

    REMOVE FIELDS

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -6,4 +6,25 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::article.article');
+//module.exports = createCoreController('api::article.article');
+
+module.exports = {
+    async find(ctx) {
+      // Finde alle Artikel
+      const entries = await strapi.services.article.find();
+  
+      // Entferne unerwünschte Felder
+      const sanitizedEntries = entries.map(entry => {
+        // Entferne die Felder aus jedem Artikel
+        delete entry.createdAt;
+        delete entry.updatedAt;
+        delete entry.publishedAt;
+  
+        return entry;
+      });
+  
+      // Gebe die bereinigten Artikel zurück
+      return sanitizedEntries;
+    }
+  };
+  
